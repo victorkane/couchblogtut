924f4a8574f0b15648e1e139e63baa43ca47f6d0
Victor Kane <victorkane@gmail.com>
2011-06-20 06:06:25 -0400

    list dummy data articles with sys

---
+++ app.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.logger());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+var articleProvider = new ArticleProvider();
+
+// Routes
+
+//app.get('/', function(req, res){
+//  res.render('index', {
+//    title: 'CouchBlog'
+//  });
+//});
+
+app.get('/', function(req, res){
+  articleProvider.findAll(function(error, docs){
+    res.send(require('sys').inspect(docs));
+  })
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d", app.address().port);
